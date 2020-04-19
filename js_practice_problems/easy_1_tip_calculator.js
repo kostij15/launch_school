4fe5676cc71611dea26b4c1bf42e279811ce5b7a
Jarrod Kostick <kostick.jarrod.s@gmail.com>
2020-04-19 19:50:58 -0400

    update

---
+++ js_practice_problems/easy_1_tip_calculator.js
@@ -0,0 +1,16 @@
+const READLINE = require('readline-sync');
+
+function total(bill, tipPercent) {
+    tipOwed = bill * tipPercent / 100;
+    bill += tipOwed
+    console.log(`The tip is \$${tipOwed.toFixed(2)}`);
+    console.log(`The total is \$${bill.toFixed(2)}`);
+}
+
+console.log('What is the bill?');
+bill = READLINE.question();
+
+console.log('What is the tip percentage?');
+tip = READLINE.question();
+
+total(Number(bill), Number(tip));
