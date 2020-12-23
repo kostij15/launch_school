67b6f7e5ed36526fb60d0cecf26ac8aec833f723
Jarrod Kostick <kostick.jarrod.s@gmail.com>
2020-12-22 21:30:05 -0500

    update

---
+++ js_130/excercises/easy/run_length_encoding.js
@@ -0,0 +1,39 @@
+"use strict";
+
+function encode(characters) {
+  let str = '';
+  let count = 1;
+
+  for (let index = 0; index < characters.length; index++) {
+
+    if (characters[index] === characters[index + 1]) {
+      count += 1;
+    } else {
+      str += `${count === 1 ? '' : count}${characters[index]}`;
+      count = 1;
+    }
+  }
+
+  return str;
+}
+
+// console.log(encodeObject("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"));
+
+function decode(characters) {
+  let str = '';
+
+  characters = characters.match(/[0-9]?[a-z]/ig);
+
+  characters.forEach(string => {
+    let stringedNum = string.match(/[0-9]/g);
+    let number = Number(stringedNum ? stringedNum[0] : 1);
+    let letter = String(string.match(/[a-z]/ig)[0]);
+
+
+    str += letter.repeat(number);
+  })
+
+  return str;
+}
+
+console.log(decode("2AB3CD4E"));
