3a1f7807a23c2a54942da5fe767cdb9cdee06b14
Jarrod Kostick <kostick.jarrod.s@gmail.com>
2021-06-21 19:33:00 -0400

    updating all new files

---
+++ js_185/lesson_1/app.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg');
+const DB_CREDENTIALS = {
+  database: 'films',
+  user: 'postgres',
+  password: 'dorraj13',
+};
+let client = new Client(DB_CREDENTIALS);
+
+async function logQuery(queryText) {
+  await client.connect();
+
+  let data = await client.query(queryText);
+
+  console.log(data.rows[2].count);
+  client.end();
+};
+
+logQuery(`SELECT count(id) FROM films
+WHERE duration < 110
+GROUP BY genre;`);
